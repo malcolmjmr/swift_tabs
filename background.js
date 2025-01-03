b9583470e47da99daad31d92a6809e9bce19005f
Malcolm Mason Rodriguez <malcolmjmr@gmail.com>
2025-01-03 00:34:41 -0500

    Enhance tab and window navigation with scroll and key bindings. Added settings storage and improved message handling for scroll events. Updated manifest for new app name and permissions.

--- background.js
+++ background.js
@@ -1,15 +1,37 @@
-let isCommandPressed = false;
+let lastKey = null;
 let scrollDelta = 0;
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.set({
+        lastKey: null,
+        scrollDelta: 0,
+        settings: {
+            keys: {
+                metaKey: 'tabs',
+                shiftKey: 'recent',
+                ctrlKey: 'windows'
+            }
+        }
+    });
+});
 
 // Listen for messages from content scripts
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
    if (message.type === 'SCROLL') {
-    handleTabSwitch(message.scrollDelta, sender.tab.id);
+    handleTabSwitch({
+        ...message,
+        tabId: sender.tab.id
+    });
   }
   return true;
 });
 
-async function handleTabSwitch(scrollUpdate, tabId) {
+async function handleTabSwitch({ scrollUpdate, key, tabId }) {
+
+    if (key != lastKey) {
+        lastKey = key;
+    }
+
     const [currentTab] = await chrome.tabs.query({ active: true, currentWindow: true });
     if (currentTab.id !== tabId) {
         return;
@@ -25,17 +47,62 @@ async function handleTabSwitch(scrollUpdate, tabId) {
         return;
     }
 
-    const tabs = await chrome.tabs.query({ currentWindow: true });
-    tabs.sort((a, b) => a.index - b.index);
+    const settings = await getSettings();
+    const keyBinding = settings.keys[key];
+
+    let tabs = [];
+    let windows = [];
+    if (keyBinding == 'tabs') {
+        tabs = await chrome.tabs.query({ currentWindow: true });
+        tabs.sort((a, b) => {
+            return a.index - b.index;
+        });
+    } else if (keyBinding == 'recent') {
+        tabs = await chrome.tabs.query({ currentWindow: true });
+        tabs.sort((a, b) => {
+            return a.lastAccessed - b.lastAccessed;
+        });
+    } else if (keyBinding == 'windows') {
+        windows = await chrome.windows.getAll();
+        windows.sort((a, b) => {
+            return a.id - b.id;
+        });
+    }
     
-    const currentIndex = tabs.findIndex(tab => tab.id === currentTab.id);
-    let newIndex = currentIndex;
-    if (scrollDelta <= scrollThreshold) {
-        newIndex = currentIndex === tabs.length - 1 ? 0 : currentIndex + 1;
-    } else if (scrollDelta >= -scrollThreshold) {
-        newIndex = currentIndex === 0 ? tabs.length - 1 : currentIndex - 1;
+    if (keyBinding == 'windows') {
+        const currentIndex = windows.findIndex(window => window.id === currentTab.windowId);
+        let newIndex = currentIndex;
+        if (scrollDelta <= scrollThreshold) {
+            newIndex = currentIndex === windows.length - 1 ? 0 : currentIndex + 1;
+        } else if (scrollDelta >= -scrollThreshold) {
+            newIndex = currentIndex === 0 ? windows.length - 1 : currentIndex - 1;
+        }
+        await chrome.windows.update(windows[newIndex].id, { focused: true });
+    } else {
+
+        const currentIndex = tabs.findIndex(tab => tab.id === currentTab.id);
+        let newIndex = currentIndex;
+        if (scrollDelta <= scrollThreshold) {
+            newIndex = currentIndex === tabs.length - 1 ? 0 : currentIndex + 1;
+        } else if (scrollDelta >= -scrollThreshold) {
+            newIndex = currentIndex === 0 ? tabs.length - 1 : currentIndex - 1;
+        }
+
+        await chrome.tabs.update(tabs[newIndex].id, { active: true });
     }
 
     scrollDelta = 0;
-    await chrome.tabs.update(tabs[newIndex].id, { active: true });
+}
+
+async function getSettings()  {
+    return await getData('settings');
+}
+
+async function storeData(data) {
+    await chrome.storage.local.set(data);
+}
+
+async function getData(key) {
+    const data = (await chrome.storage.local.get([key])) ?? {};
+    return data[key];
 }
--- content.js
+++ content.js
@@ -1,10 +1,11 @@
 // Listen for wheel event to detect scrolling
 document.addEventListener('wheel', (event) => {
     // Send tab switch message to background script
-    if (event.metaKey) {  
+    if (event.metaKey || event.shiftKey || event.ctrlKey) {  
         chrome.runtime.sendMessage({ 
             type: 'SCROLL',
-            scrollDelta: event.deltaY
+            scrollDelta: event.deltaY,
+            key: event.metaKey ? 'metaKey' : event.shiftKey ? 'shiftKey' : 'ctrlKey'
         });  
 
         // Prevent default scrolling behavior
